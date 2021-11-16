c8fd8d6c6314870ab4d9c924397e6f37a0b33dd3
HS <mukul.singh@habilelabs.io>
2021-11-16 17:02:27 +0530

    2 folder added

---
+++ project/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+//import Restaurant from './component/Basics/Restaurant';
+//import UseState from './component/Hooks/useState';
+//import UseEffect from './component/Hooks/useEffect'
+//import UseReducer from './component/Hooks/useReducer';
+import Todo from './component/todoReact/todo';
+
+const App = () => {
+  return (
+    <div>
+      <Todo />
+      
+      
+    </div>
+  )
+}
+
+export default App
---
+++ project/src/component/Basics/Restaurant.js
@@ -0,0 +1,39 @@
+import React, { useState } from 'react'
+import "./style.css"
+import Menu from "./menuApi"
+import MenuCard from './MenuCard';
+import Navbar from './Navbar';
+
+const uniqueList = [
+    ...new Set(
+        Menu.map((curlItem) => {
+            return curlItem.category;
+        })
+    ),
+    "All",
+];
+
+const Restaurant = () => {
+    const [menuData, setMenuData] = useState(Menu);
+    const [menuList, setMenuList] = useState(uniqueList);
+   
+    const filterItem = (category) => {
+        if(category === "All"){
+            setMenuData(Menu);
+            return;
+        }
+        const updatedList = Menu.filter((curlItem) => {
+            return curlItem.category === category;
+        });
+        setMenuData(updatedList);
+    };
+    return (
+        <>
+            
+            <Navbar filterItem={filterItem} menuList={menuList}/>
+          <MenuCard menuData={menuData} /> 
+        </>
+    )
+}
+
+export default Restaurant
